bdacfec58beb5001444c4c3bf61dbe16918bf7c2
AN <arngu01@outlook.com>
2026-09-10 23:34:16 +0100

    Add logic to check for win
    Added a check to see if a player wins.

--- script.js
+++ script.js
@@ -6,15 +6,51 @@ function createBoard() {
     }
 
     function updateBoard(pos1, pos2, symbol) {
-        board[pos1][pos2] = symbol
+        if (board[pos1][pos2] === null) {
+            board[pos1][pos2] = symbol
+        };
+        
+        console.log(checkWin(pos1,pos2,symbol));
+
+        console.log(board);
     }
 
     function restartBoard() {
         board = [[null,null,null],[null,null,null],[null,null,null]]
     }
 
-    function checkWin() {
-        
+    function checkWin(pos1, pos2, symbol) {
+        let win = false;
+        for (let i = 0; i < 3; i++){
+            if (board[pos1][i] !== symbol) {
+                break;
+            };
+
+            if (i === 2) {
+                win = true;
+            };
+        };
+
+        for (let i = 0; i < 3; i++){
+            if (board[i][pos2] !== symbol) {
+                break;
+            };
+
+            if (i === 2) {
+                win = true;
+            };
+        };
+
+        if (pos1 === 0 || pos1 === 2 || pos2 === 0 || pos2 === 2) {
+            if (board[0][0] === symbol && board[1][1] === symbol && board[2][2] === symbol) {
+                win = true;
+            };
+            if (board[0][2] === symbol && board[1][1] === symbol && board[2][0] === symbol) {
+                win = true;
+            };
+        };
+
+        return win;
     }
 
     return {getBoard, updateBoard, restartBoard};
@@ -22,11 +58,11 @@ function createBoard() {
 
 function Player(symbol) {
     this.symbol = symbol;
-    this.points = 0
+    this.points = 0;
 }
 
 Player.prototype.win = function () {
-    this.points ++
+    this.points ++;
 }
 
 function TicTacToeGame() {
