ebb33b4061e3058e6109873f73e86871c172298c
AN <arngu01@outlook.com>
2026-09-12 22:04:04 +0100

    Create an IIFE for game
    Created a IIFE for the tictactoe game to control game logic.

--- script.js
+++ script.js
@@ -9,8 +9,6 @@ function createBoard() {
         if (board[pos1][pos2] === null) {
             board[pos1][pos2] = symbol
         };
-        
-        console.log(checkWin(pos1,pos2,symbol));
 
         console.log(board);
     }
@@ -53,24 +51,63 @@ function createBoard() {
         return win;
     }
 
-    return {getBoard, updateBoard, restartBoard};
+    return {getBoard, updateBoard, restartBoard, checkWin};
 }
 
-function Player(symbol) {
-    this.symbol = symbol;
+function Player(name, marker) {
+    this.name = name
+    this.marker = marker;
     this.points = 0;
 }
 
-Player.prototype.win = function () {
+Player.prototype.win = function() {
     this.points ++;
 }
 
-function TicTacToeGame() {
+const tictactoeGame = (() => {
+    let tictactoeBoard;
+    let player1;
+    let player2;
 
-}
+    const startGame = (p1Name, p2Name) => {
+        tictactoeBoard = createBoard();
 
-const tictactoe = createBoard()
+        player1 = new Player(p1Name,"x");
 
-const player1 = new Player("x");
+        player2 = new Player(p2Name, "o");
+    };
 
-const player2 = new Player("O")
+    const playTurn = (pos, player) => {
+        tictactoeBoard.updateBoard(pos[0], pos[1], player.marker)
+
+        if (tictactoeBoard.checkWin(pos[0], pos[1], player.marker)) {
+            console.log("win")
+            player.win()
+            tictactoeBoard.restartBoard()
+            return true
+        }
+
+        return false
+    }
+
+    const playRound = (player1, player2) => {
+        let turn = 0;
+        let win = false;
+        while (win === false) {
+            const pos1 = +prompt("Enter pos1: ");
+            const pos2 = +prompt("Enter pos2: ");
+
+            win = playTurn([pos1, pos2], player1)
+        }
+    }
+
+    const getPlayer1 = () => player1;
+    const getPlayer2 = () => player2;
+    const getGameBoard = () => tictactoeBoard; 
+
+    return {getGameBoard, getPlayer1, getPlayer2, startGame, playRound};
+})();
+
+tictactoeGame.startGame("p1", "p2");
+tictactoeGame.playRound(tictactoeGame.getPlayer1(), tictactoeGame.getPlayer2())
+
